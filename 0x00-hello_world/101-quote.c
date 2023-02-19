9383cd76f4af92c212528afe39f1311fb9a4159a
alphawga <alphawga@gmail.com>
2023-02-18 21:00:12 -0800

    D

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,8 @@
  *  Return: 1 (sucessful)
  */
 int main(void)
+	
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")
-	return (1)
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
