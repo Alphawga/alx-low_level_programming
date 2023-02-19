fec7ceecb1882ea8eab5515f33125075aa074f3c
alphawga <alphawga@gmail.com>
2023-02-18 20:03:11 -0800

    Dd

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A code to print a line of sentence
+ *Return: 0 usimg a grammer
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
