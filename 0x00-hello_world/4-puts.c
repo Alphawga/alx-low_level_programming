492c211c7f5f44d22cd9c33fdd1b300ee3d77bea
alphawga <alphawga@gmail.com>
2023-02-18 19:47:01 -0800

    kkdd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
-
+/**
+ * main -A program that prints a line 
+ *
+ * Return: 0(sucess)
+*/
 int main(void)
 {
-	return("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle\n");
+	
+	return (0);
 }
