743c433233a119fc8d2300a708a57f324f02cbe3
alphawga <alphawga@gmail.com>
2023-02-20 19:11:31 -0800

    g

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,17 +18,17 @@ int main(void)
 	last_digit = n % 10;
 	if (last_digit > 5)
 	{
-		printf("Last digit of %d is %d", n, last_digit);
+		printf("Last digit of %d is %d ", n, last_digit);
 			       printf("and is greater than 5\n");
 	}
 	else if (last_digit == 0)
 	{
-		printf("Last digit of %d is %d", n, last_digit);
+		printf("Last digit of %d is %d ", n, last_digit);
 			       printf("and is 0\n");
 	}
 	else
 	{
-		printf("Last digit of %d is %d", n, last_digit);
+		printf("Last digit of %d is %d ", n, last_digit);
 		       printf("and is less than 6 and not 0\n");
 	}
 	return (0);
