a8bd102c0c8e3734953be14ee0121e1876a525f7
alphawga <alphawga@gmail.com>
2023-02-20 19:35:53 -0800

    f

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *  main - Print alphabet
+ *
+ *   Return: 0(sucess)
+ */
+
+int main(void)
+{
+        char ch = "abcdfghijklmnoprstuvwxyz";
+
+        putchar(ch);
+        putchar("\n");
+
+        return (0);
+
+}
