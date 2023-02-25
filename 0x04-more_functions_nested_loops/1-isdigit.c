1f6ac65ba3ca3e7978c569c363c63b70b4a77002
alphawga <alphawga@gmail.com>
2023-02-24 19:58:53 -0800

    d

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,13 @@
 #include "main.h"
 
 /**
- *  main - A functions that checks
- *  @c is a argument
- *
- *  Return: 0 or 1
- */
-
+* _isdigit - write a function that check for a digit (0 through 9).
+* @c: char to check
+* Return:  0 or 1
+**/
 int _isdigit(int c)
 {
-	if (c >=0 && c <= -9)
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - A function to multiply
+ * @a: first integer
+ * @b: second integer
+ *
+ *  Return: 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
